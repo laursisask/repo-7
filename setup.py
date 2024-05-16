e85bb4a4f63dd504f0560cfb02a49c7bc46bef28
Sergio Tejón <stejon@freepik.com>
2024-05-16 21:44:03 +0200

    feat(package): Rename package name and update description
    Update pyproject.toml and setup.py files

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='model_download_package',
-    version='0.1',
-    description='A package for downloading models',
+    name='comfy_model_download',
+    description='A package for downloading models for ComfyUI',
     packages=find_packages(),
 )
 
