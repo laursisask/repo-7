a2a3650f9de256d1a67509740cb8d34dc18f221f
Sergio Tejón <stejon@freepik.com>
2024-05-17 00:00:09 +0200

    revert(setup.py): Remove deleted setup.py file

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='comfy_model_download',
-    description='A package for downloading models for ComfyUI',
-    packages=find_packages(),
-)
-
